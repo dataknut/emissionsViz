1cca733c3a5e8de35bd2bdaf1f4f9238994fee13
Ben Anderson <dataknut@icloud.com>
2021-12-10 14:47:31 +0000

    added useful functions

--- R/functions.R
+++ R/functions.R
@@ -1,1 +1,12 @@
 # re-usable functions
+
+library(flextable)
+makeFlexTable <- function(df, cap = "caption"){
+  # makes a pretty flextable - see https://cran.r-project.org/web/packages/flextable/index.html
+  ft <- flextable::flextable(df)
+  ft <- flextable::colformat_double(ft, digits = 1)
+  ft <- flextable::fontsize(ft, size = 9)
+  ft <- flextable::fontsize(ft, size = 10, part = "header")
+  ft <- flextable::set_caption(ft, caption = cap)
+  return(flextable::autofit(ft))
+}
