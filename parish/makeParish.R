365d3aee0d0cd5b4cc1417fe3501ead4f5deac4d
Ben Anderson <dataknut@icloud.com>
2021-12-10 14:47:43 +0000

    added makefile for parishes

---
+++ parish/makeParish.R
@@ -0,0 +1,28 @@
+# makes pdfs
+
+# Packages ----
+library(here)
+
+# Functions ----
+source(here::here("R", "functions.R"))
+
+makeReport <- function(p, title, subtitle, authors, la){
+  # default = whatever is set in yaml
+  rmarkdown::render(input = here::here("parish", "parish_Template.Rmd"),
+                    params = list(title = title,
+                                  subtitle = subtitle,
+                                  parish = parish),
+                    output_file = paste0(here::here("docs/parish_"), parish)
+  )
+}
+
+# Fails for parishes inside unitary authorities due to look-up table (see rmd)
+pList <- c("Framlingham", "Badingham", "Botley")
+
+for(p in pList){
+  title <- p
+  subtitle <- "Estimated Parish Level Greenhouse Gas (GHG) Emissions 2017/18"
+  parish <- p
+  la <- la
+  makeReport(parish, title, subtitle)
+}
